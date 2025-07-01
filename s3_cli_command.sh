f1ddd64f888a838a5e68164b62a34ef6f274059e
Ashna <65767889+AshnaVirdikar@users.noreply.github.com>
2025-07-01 10:50:37 +0530

    Update s3_cli_command.sh

--- s3_cli_command.sh
+++ s3_cli_command.sh
@@ -3,22 +3,23 @@ aws s3 ls # this will start the process in your CLI
 # to clean 
 cls
 #2. To upload complete folder use this command: 
-aws s3 sync "path to folder on your local device" s3://Bucket_name
+aws s3 sync "path to folder on your local device" s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics_reference_data/
 # To copy all JSON Reference data to same location:
 aws s3 cp . s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
 
+s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics_reference_data/
 
 #3. Print all files uplaoded to S3 Bucket in AWS
-aws s3 ls s3:// Bucket_name
+aws s3 ls s3:// s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics_reference_data/
 
 # To copy all data files to its own location, following Hive-style patterns:
-aws s3 cp CAvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=ca/
-aws s3 cp DEvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=de/
-aws s3 cp FRvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=fr/
-aws s3 cp GBvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=gb/
-aws s3 cp INvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=in/
-aws s3 cp JPvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=jp/
-aws s3 cp KRvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=kr/
-aws s3 cp MXvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=mx/
-aws s3 cp RUvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=ru/
-aws s3 cp USvideos.csv s3://de-on-youtube-raw-useast1-dev/youtube/raw_statistics/region=us/
+aws s3 cp CAvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://dataeng-on-youtube-raw-apsouth-1-dev/Youtube/raw_statistics/region=us/
